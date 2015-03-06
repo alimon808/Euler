922fcaf005c8e850ac09b1427aed323bb47bcb71
Adrian Limon <alimon.secure@live.com>
2015-03-05 23:35:01 -0800

    sum of even fibonacci terms for a given N

---
+++ Euler/Fibonacci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Euler
+{
+    public class Fibonacci
+    {
+        public static ulong SumOfEvenTerms(ulong n)
+        {
+            var sequence = Sequence(n);
+            ulong sum = 0UL;
+            foreach (var num in sequence)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+            return sum;
+        }
+
+        public static List<ulong> Sequence(ulong n)
+        {
+            var sequence = new List<ulong>(){1UL, 2UL};
+            var sum = 0UL;
+            while (sum < n)
+            {
+                var count = sequence.Count;
+                sum = sequence[sequence.Count - 1] + sequence[sequence.Count - 2];
+                sequence.Add(sum);
+            }
+            return sequence;
+        }
+    }
+}
---
+++ EulerTests/FibonacciTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Euler;
+using NUnit.Framework;
+
+namespace EulerTests
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        
+        [Test]
+        public void ShouldReturn10ForSumOfEvenFibonacciTermsThatAreLessThan10()
+        {
+            var sum = Fibonacci.SumOfEvenTerms(10UL);
+            Assert.That(sum, Is.EqualTo(10));
+        }
+
+        [Test]
+        public void ShouldReturn4613732ForSumOfEvenFibonacciThatAreLessThanFourMillion()
+        {
+            var sum = Fibonacci.SumOfEvenTerms(4000000UL);
+            Assert.That(sum, Is.EqualTo(4613732));
+        }
+    }
+}
