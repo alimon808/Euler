79162e7fd1714367ab4c2d237a7e3e41c22a9f7d
Adrian Limon <alimon.secure@live.com>
2015-03-05 23:17:04 -0800

    sum of multiples of 3 and 5 for given N

---
+++ Euler/MultipleOfThreeAndFive.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Euler
+{
+	public class MultipleOfThreeAndFive
+	{
+		public static int Sum(int n)
+		{
+			var multiples = Multiples(n);
+			int sum = 0;
+			foreach (var m in multiples)
+			{
+				sum += m;
+			}
+			return sum;
+		}
+
+		private static List<int> Multiples(int n)
+		{
+			var list = new List<int>();
+			for (int i = 1; i < n; i++)
+			{
+				if (i % 3 == 0 || i % 5 == 0)
+				{
+					list.Add(i);
+				}
+			}
+			return list;
+		}
+	}
+}
---
+++ EulerTests/MultipleOfTreeAndFiveTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Euler;
+using NUnit.Framework;
+
+namespace EulerTests
+{
+    [TestFixture]
+    public class MultipleOfTreeAndFiveTests
+    {
+        // 3, 5, 6, 9, 10, 12 = 60
+        [Test]
+        public void ShouldReturn45ForSumOf15()
+        {
+            var sum = MultipleOfThreeAndFive.Sum(15);
+            Assert.That(sum, Is.EqualTo(45));
+        }
+
+        [Test]
+        public void ShouldReturn233168ForSumOf1000()
+        {
+            var sum = MultipleOfThreeAndFive.Sum(1000);
+            Assert.That(sum, Is.EqualTo(233168));
+        }
+    }
+}
