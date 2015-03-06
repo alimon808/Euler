94832861de5c8c40fa7363a5784abf01493e517b
alimon808 <alimon808@gmail.com>
2015-03-06 02:11:02 -0800

    problem 5 - smallest multiple

--- Euler/EulerHelper.cs
+++ Euler/EulerHelper.cs
@@ -18,7 +18,7 @@ namespace Euler
             {
                 return false;
             }
-            for (ulong i = 5; i * i <= n; i+=6)
+            for (ulong i = 5; i * i <= n; i += 6)
             {
                 if (n % i == 0 || n % (i + 2) == 0)
                 {
@@ -31,7 +31,7 @@ namespace Euler
         public static bool IsPalindrome(ulong n)
         {
             var arr = n.ToString().ToCharArray();
-            int firstIndex= 0;
+            int firstIndex = 0;
             int lastIndex = arr.Length - 1;
 
             while (firstIndex < lastIndex)
@@ -45,6 +45,48 @@ namespace Euler
             }
 
             return true;
+        }
+
+        public static ulong SmallestMultipleBruteForce(ulong n)
+        {
+            ulong smallestMultiple = n;
+            for (ulong i = n; i > 1; i--)
+            {
+                if (smallestMultiple % i != 0)
+                {
+                    smallestMultiple++;
+                    i = n + 1;
+                }
+            }
+            return smallestMultiple;
+        }
+
+        public static int SmallestMultiple(int k)
+        {
+            int N = 1;
+            int i = 1;
+            bool check = true;
+            var limit = Math.Sqrt(k);
+            int[] primes = new int[]{1, 2, 3, 5, 7, 11, 13, 17, 19, 23};
+            int[] a = new int[9];
+            while (primes[i] <= k)
+            {
+                a[i] = 1;
+                if (check)
+                {
+                    if (primes[i] <= limit)
+                    {
+                        a[i] = (int)Math.Floor(Math.Log(k) / Math.Log(primes[i]));
+                    }
+                    else
+                    {
+                        check = false;
+                    }
+                }
+                N = N * (int)Math.Pow(primes[i], a[i]);
+                i = i + 1;
+            }
+            return N;
         }
     }
 }
---
+++ EulerTests/SmallestMultipleTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EulerTests
+{
+    [TestFixture]
+    public class SmallestMultipleTests
+    {
+        [Test]
+        public void ShouldReturnSmallestMultipleUsingBruteForce()
+        {
+            var smallestMultiple = Euler.EulerHelper.SmallestMultipleBruteForce(10);
+            Console.WriteLine(smallestMultiple);
+            Assert.That(smallestMultiple, Is.EqualTo(2520));
+
+            smallestMultiple = Euler.EulerHelper.SmallestMultipleBruteForce(20);
+            Console.WriteLine(smallestMultiple);
+            Assert.That(smallestMultiple, Is.EqualTo(232792560));
+        }
+
+        [Test]
+        public void ShouldReturnSmallestMultiple()
+        {
+            var smallestMultiple = Euler.EulerHelper.SmallestMultiple(10);
+            Console.WriteLine(smallestMultiple);
+            Assert.That(smallestMultiple, Is.EqualTo(2520));
+
+            smallestMultiple = Euler.EulerHelper.SmallestMultiple(20);
+            Console.WriteLine(smallestMultiple);
+            Assert.That(smallestMultiple, Is.EqualTo(232792560));
+        }
+    }
+}
