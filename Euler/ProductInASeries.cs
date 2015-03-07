573c9d30b697c69b8e439b86d556a77f3ca648cb
alimon808 <alimon808@gmail.com>
2015-03-06 21:14:57 -0800

    problem 8: largest product in a series

---
+++ Euler/ProductInASeries.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Euler
+{
+    public class ProductInASeries
+    {
+        public static ulong Largest(string strN, int adjacentDigits)
+        {
+            int currentIndex = 0;
+            ulong maxProduct = 0;
+
+            while (currentIndex + adjacentDigits < strN.Length)
+            {
+                var str = strN.Substring(currentIndex, adjacentDigits);
+                var product = Product(str);
+                if (maxProduct < product)
+                {
+                    maxProduct = product;
+                }
+                currentIndex++;
+            }
+            
+            return maxProduct;
+        }
+
+        private static ulong Product(string num)
+        {
+            ulong product = 1;
+            foreach (var n in num)
+            {
+                int temp = int.Parse(n.ToString());
+                product = product * (ulong)temp;
+            }
+            return product;
+        }
+    }
+}
---
+++ EulerTests/PrimeTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace EulerTests
+{
+    [TestFixture]
+    public class PrimeTests
+    {
+        [TestCase(6)]
+        [TestCase(10001)]
+        public void ShouldReturnTheNthPrime(int n)
+        {
+            var count = 0;
+            var prime = 1;
+            var i = 1;
+            while (count != n)
+            {
+                i++;
+                if (Euler.EulerHelper.IsPrime((ulong)i))
+                {
+                    prime = i;
+                    count++;
+                }
+            }
+            Console.WriteLine(prime);
+        }
+    }
+}
