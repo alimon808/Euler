e39feb76d93488c67a3fa2f59e90ed540432458a
alimon808 <alimon808@gmail.com>
2015-03-07 20:28:18 -0800

    create pythagorean class with Triples static with method

---
+++ Euler/Pythagorean.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Euler
+{
+    public class Pythagorean
+    {
+        public static List<int[]> Triplets(int n)
+        {
+            var list = new List<int[]>();
+
+            int c = 997;
+            int b = 2;
+            int a = 1;
+            while (b < c)
+            {
+                while (a < b)
+                {
+                    if ((Math.Pow(a, 2) + Math.Pow(b, 2)) == Math.Pow(c, 2))
+                    {
+                        list.Add(new[] { a, b, c });
+                    }
+                    a++;
+                    c--;
+                }
+                c = c + a - 2;
+                a = 1;
+                b++;
+            }
+
+            return list;
+        }
+
+        //public static List<int[]> Triplets2(int n)
+        //{
+        //    int a = 0;
+        //    int b = 0;
+        //    int c = 0;
+        //    var k = 0;
+        //    var sm = 0;
+        //    var list = new List<int[]>();
+        //    var n2 = n / 2;
+        //    var mLimit = Math.Ceiling(Math.Abs(Math.Sqrt(n2))) - 1;
+        //    for (int m = 2; m < mLimit; m++)
+        //    {
+        //        if (n2 % m == 0)
+        //        {
+        //            sm = n2 / m;
+        //            while (sm % 2 == 0)
+        //            {
+        //                sm = sm / 2;
+        //            }
+
+        //            if (m % 2 == 1)
+        //            {
+        //                k = m + 2;
+        //            }
+        //            else
+        //            {
+        //                k = m + 1;
+        //            }
+
+        //            while (k < 2 * m && k <= sm)
+        //            {
+        //                if (sm % k == 0 && GCD(k, m) == 1 )
+        //                {
+        //                    var d = n2 / (k * m);
+        //                    n = k - m;
+        //                    a = d * (m * m - n * n);
+        //                    b = 2 * d * m * n;
+        //                    c = d * (m * m + n * n);
+        //                    Console.WriteLine("{0}, {1}, {2}", a, b, c);
+        //                    list.Add(new[] { a, b, c });
+        //                }
+        //                k = k + 2;
+        //            }
+        //        }
+        //    }
+        //    return list;
+        //}
+
+        //private static int GCD(int k, int m)
+        //{
+
+        //    return 1;
+        //}
+    }
+}
--- EulerTests/SpecialPythagoreanTripletTests.cs
+++ EulerTests/SpecialPythagoreanTripletTests.cs
@@ -10,26 +10,7 @@ namespace EulerTests
         [Test]
         public void ShouldReturnOneTriplet()
         {
-            var list = new List<int[]>();
-
-            int c = 997;
-            int b = 2;
-            int a = 1;
-            while (b < c)
-            {
-                while (a < b)
-                {
-                    if ((Math.Pow(a, 2) + Math.Pow(b, 2)) == Math.Pow(c, 2))
-                    {
-                        list.Add(new[] { a, b, c });
-                    }
-                    a++;
-                    c--;
-                }
-                c = c + a - 2;
-                a = 1;
-                b++;
-            }
+            var list = Euler.Pythagorean.Triplets(1000);
 
             foreach (var item in list)
             {
@@ -41,5 +22,11 @@ namespace EulerTests
             var abc = list[0][0] * list[0][1] * list[0][2];
             Assert.That(abc, Is.EqualTo(31875000));
         }
+
+        //[Test]
+        //public void ShouldReturnPythagoreanTriplet()
+        //{
+
+        //}
     }
 }
