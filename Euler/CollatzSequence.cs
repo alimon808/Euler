19e7c5c68a592ae6e18b2ce29cc346bbe16a4f01
alimon808 <alimon808@gmail.com>
2015-04-04 15:00:23 -0700

    problem 14: Collatz Sequence

---
+++ Euler/CollatzSequence.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Euler
+{
+    // n -> n/2 (even)
+    // n -> 3n + 1 (odd)
+    // break when n = 1
+    public class CollatzSequence
+    {
+        public List<double> Compute(double n)
+        {
+            var sequence = new List<double>();
+            while (n > 1)
+            {
+                sequence.Add(n);
+                if (n % 2 == 0)
+                {
+                    n = n / 2;
+                }
+                else
+                {
+                    n = 3 * n + 1;
+                }
+            }
+            sequence.Add(1);
+
+            return sequence;
+        }
+
+        public double LargestSequence(double n)
+        {
+            double count = 0;
+            double maxN = n;
+            while (n > 1)
+            {
+                var sequence = this.Compute(n);
+                if (sequence.Count > count)
+                {
+                    count = sequence.Count;
+                    maxN = n;
+                    Console.WriteLine("{0} : {1}", n, count);
+                }
+                n--;
+            }
+
+            return maxN;
+        }
+    }
+}
---
+++ EulerTests/CollatzSequenceTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Euler;
+using NUnit.Framework;
+
+namespace EulerTests
+{
+    [TestFixture]
+    public class CollatzSequenceTests
+    {
+        [Test]
+        public void ShouldReturnTenTermsForNEqualThirteen()
+        {
+            var max = 13;
+            var sut = new CollatzSequence();
+            List<double> sequence = sut.Compute(max);
+            Assert.That(sequence.Count, Is.EqualTo(10));
+            //Console.WriteLine(sequence.Count);
+            //foreach (var item in sequence)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+
+        [Test]
+        public void ShouldTestMaxNUnderOneMillion()
+        {
+            var n = 837799;
+            var sut = new CollatzSequence();
+            List<double> sequence = sut.Compute(n);
+            Assert.That(sequence.Count, Is.EqualTo(525));
+            foreach (var item in sequence)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        [Test]
+        public void ShouldReturnTheLargestSequenceCountForForANumberBelowOneMillion()
+        {
+            var n = 1000000;
+            var sut = new CollatzSequence();
+            var result = sut.LargestSequence(n);
+            Console.WriteLine(result);
+            Assert.That(result, Is.EqualTo(837799));
+        }
+    }
+}
