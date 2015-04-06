8c78a3255c72fcb0533f700f5907b522d6c57591
alimon808 <alimon808@gmail.com>
2015-04-05 22:20:29 -0700

    Problem 16: Power digit sum solved

---
+++ EulerTests/PowerDigitSumTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EulerTests
+{
+    [TestFixture]
+    public class PowerDigitSumTests
+    {
+        [Test]
+        public void ShouldSumTwoToThePowerOfFifteen()
+        {
+            double val = Math.Pow(2, 15);
+            int sum = 0;
+            foreach (var c in val.ToString())
+            {
+                sum += (int)Char.GetNumericValue(c);
+                //Console.WriteLine("{0} : {1}", c, sum);
+            }
+            Assert.That(sum, Is.EqualTo(26));
+        }
+
+        // Problem 16: Power digit sum
+        [Test]
+        public void ShouldSumTwoToThePowerOfOneThousand()
+        {
+            BigInteger val = (BigInteger)Math.Pow(2, 1000);
+            int sum = 0;
+            foreach (var c in val.ToString())
+            {
+                sum += (int)Char.GetNumericValue(c);
+            }
+            //Console.WriteLine(sum);
+            Assert.That(sum, Is.EqualTo(1366));
+        }
+    }
+}
